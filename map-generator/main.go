55898b4ecb96c00413b5600d9a1fca3f4b7ff536
Jeff Lawlor <jeff.lawlor+ideas@gmail.com>
2026-05-30 14:52:55 -0700

    Now generating maps from Go rather than the map building web site.

--- map-generator/main.go
+++ map-generator/main.go
@@ -98,6 +98,7 @@ var maps = []struct {
 	{Name: "twolakes"},
 	{Name: "world"},
 	{Name: "yenisei"},
+	{Name: "aintnobodyherebutuschickens"},
 	{Name: "big_plains", IsTest: true},
 	{Name: "half_land_half_ocean", IsTest: true},
 	{Name: "ocean_and_land", IsTest: true},
